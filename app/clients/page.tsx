8caed992e39be61494209c34120215244ac5715c
HenshawIII <Henshawimmanuel@gmail.com>
2026-01-10 15:50:11 +0100

    midas

--- app/clients/page.tsx
+++ app/clients/page.tsx
@@ -32,7 +32,7 @@ export default function Clients() {
           </FadeInOnScroll>
 
           {/* Clients Grid */}
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-12">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1 md:gap-12">
             {clients.map((client, index) => (
               <FadeInOnScroll
                 key={index}
